e50b9801a2efa659f59fed8307124942287ded99
Seb Stanbrough <STA0035@fairhillshs.vic.edu.au>
2024-12-15 13:45:54 +1100

    Sample script for running window

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,34 @@
 #include <SDL3/SDL.h>
 
 int main() {
-    SDL_Window *window = SDL_CreateWindow("Blython", 500, 500, 0);
-    
+    std::cout << "Runs at all?\n";
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cout << "SDL_Video failed to init: " << SDL_GetError() << '\n';
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("Blython", 500, 500, SDL_WINDOW_FULLSCREEN);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, "renderer");
+    SDL_Event event;
+
+    bool running = true;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_EVENT_QUIT:
+                    running = false;
+                    break;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderClear(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
